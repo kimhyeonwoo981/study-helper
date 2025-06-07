4a0f6e3a9c1bd38a6e7a5e69e75d4e16bcfe7622
kimhyeonwoo981 <climber0519@gmail.com>
2025-06-08 04:53:52 +0900

    Fix: Next.js Page 타입 호환 수정

--- src/app/question-list/[subject]/page.tsx
+++ src/app/question-list/[subject]/page.tsx
@@ -1,5 +1,8 @@
 import dynamic from 'next/dynamic';
 
+// 👇 이 줄이 핵심!
+export const dynamicParams = false;
+
 const ClientPage = dynamic(() => import('./ClientPage'), { ssr: false });
 
 export default function Page() {
